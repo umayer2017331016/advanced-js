585532501bdaccb704941d2b1e3361dab60ec110
unknown <umayercse16@gmail.com>
2022-03-01 15:51:33 +0600

    Advanced Javascript

---
+++ inch-to-fit.js
@@ -0,0 +1,9 @@
+function inchToFit(inch){
+    var fit = inch/12;
+    return fit;
+}
+console.log(inchToFit(156));
+console.log(inchToFit(256));
+console.log(inchToFit(356));
+console.log(inchToFit(456));
+console.log(inchToFit(556));
